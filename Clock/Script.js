734f5831ef07080f25dbd41269bf6da110da561c
Armid Alizadeh <107582502+ARMID2003@users.noreply.github.com>
2023-09-15 02:40:07 +0330

    Uhrzeit

---
+++ Clock/Script.js
@@ -0,0 +1,23 @@
+function displayTime (){
+var dateTime= new Date();
+var hrs = dateTime.getHours();
+var mins = dateTime.getMinutes();
+var secs = dateTime.getSeconds();
+var Session = document.getElementById('Session');
+
+// if (hrs >12){
+//     hrs= hrs -12;
+// }
+
+if (hrs>12) {
+    Session.innerHTML='PM'
+} else {
+    Session.innerHTML='AM'
+}
+
+document.getElementById('Stunde').innerHTML=hrs + 1
+document.getElementById('Minute').innerHTML=mins
+document.getElementById('Sekunde').innerHTML=secs
+}
+
+setInterval(displayTime,10);
